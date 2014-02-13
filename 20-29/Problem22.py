b89aad97928886305c4787cb3a7e8880316cbfe7
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-13 15:52:01 -0500

    Problem 22

---
+++ 20-29/Problem22.py
@@ -0,0 +1,28 @@
+from time import clock
+
+
+def timer(function):
+    def wrapper(*args, **kwargs):
+        start = clock()
+        print(function(*args, **kwargs))
+        print("Solution took: %f seconds." % (clock() - start))
+    return wrapper
+
+
+@timer
+def find_answer():
+    with open("names.txt") as f:
+        names = [name[1:-1] for name in f.read().split(",")]
+
+    names.sort()
+
+    total = 0
+    for index, name in enumerate(names):
+        total += (index + 1) * sum(map(lambda x: ord(x) - 64, name))
+
+    return total
+
+
+if __name__ == "__main__":
+    find_answer()
+
