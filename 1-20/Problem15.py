99ccc62860cf58ba60dbe6e36b943979953120af
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-13 09:39:05 -0500

    Problem 15 added

---
+++ 1-20/Problem15.py
@@ -0,0 +1,25 @@
+from time import clock
+
+
+def timer(function):
+    def wrapper(*args, **kwargs):
+        start = clock()
+        print(function(*args, **kwargs))
+        print("Solution took: %f seconds." % (clock() - start))
+    return wrapper
+
+
+@timer
+def find_answer():
+    # Combinatorics Approach (2n choose n)
+    paths = 1
+    for i in range(20):
+        paths *= 2 * 20 - i
+        paths //= i + 1
+
+    return paths
+
+
+if __name__ == "__main__":
+    find_answer()
+
