99756301001f6695df1dfe14a08c4eb74fc71d57
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-13 09:50:50 -0500

    Problem 16 added

---
+++ 1-20/Problem16.py
@@ -0,0 +1,20 @@
+from time import clock
+
+
+def timer(function):
+    def wrapper(*args, **kwargs):
+        start = clock()
+        print(function(*args, **kwargs))
+        print("Solution took: %f seconds." % (clock() - start))
+
+    return wrapper
+
+
+@timer
+def find_answer():
+    return sum([int(x) for x in str(2 ** 1000)])
+
+
+if __name__ == "__main__":
+    find_answer()
+
