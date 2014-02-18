6648920421a0d7ecc6dfcf32e9b237317c07c4d3
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-17 19:06:22 -0500

    Problem 27 added.

--- 1-99/20-29/Problem27.py
+++ 1-99/20-29/Problem27.py
@@ -1,4 +1,5 @@
 from time import clock
+from itertools import count
 
 
 def timer(function):
@@ -12,7 +13,37 @@ def timer(function):
 
 @timer
 def find_answer():
-    return 5
+    primes = sieve(1000000)
+    primes.remove(0)
+    poly = lambda x, y, z: z**2 + x*z + y
+    maximum = 0
+    for a in range(-1000, 1001):
+        for b in range(-1000, 1001):
+            for n in count(0):
+                if poly(a, b, n) not in primes:
+                    if n > maximum:
+                        product = a * b
+                        maximum = n
+                    break
+
+    return product
+
+
+def sieve(upperlimit):
+    l = list(range(2, upperlimit + 1))
+
+    # Do p = 2 first so we can change step size to 2*p below
+    for i in range(4, upperlimit + 1, 2):
+        l[i - 2] = 0
+
+    for p in l:
+        if p ** 2 > upperlimit:
+            break
+        elif p:
+            for i in range(p * p, upperlimit + 1, 2 * p):
+                l[i - 2] = 0
+
+    return set(l)
 
 
 if __name__ == "__main__":
