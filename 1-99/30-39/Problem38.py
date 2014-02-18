feed29ab3bb62d1f340512af1d8ac41514fd14f8
Trace Pillars <trace.pillars@e7solutions.com>
2014-02-17 20:40:32 -0500

    Problem 38 added

--- 1-99/30-39/Problem38.py
+++ 1-99/30-39/Problem38.py
@@ -12,6 +12,12 @@ def timer(function):
 
 @timer
 def find_answer():
+    target = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(9387, 9233, -1):
+        current = [x for x in str(i)] + [x for x in str(i * 2)]
+        current.sort()
+        if current == target:
+            return str(i) + str(i * 2)
     return 5
 
 
